e21ae6b99bd21451359c17d3b8b9624756ebf1be
zxh1786619259@163.com <zxh1786619259@163.com>
2019-10-20 23:00:39 +0800

    commit first

---
+++ order/src/main/java/com/tianzhuan/order/Order_MainActivity.java
@@ -0,0 +1,13 @@
+package com.tianzhuan.order;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Order_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order__main);
+    }
+}
